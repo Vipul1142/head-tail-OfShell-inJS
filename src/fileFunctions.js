e975818d1fa4e151e95e3309ff9f471066d49975
Vipul Jha <vipuljha1142@gmail.com>
2020-04-15 12:04:02 +0530

    code refactored

--- src/fileFunctions.js
+++ src/fileFunctions.js
@@ -1,13 +1,13 @@
-let filesys = require("fs");
+let files = require("fs");
 
 //function to check if a given file exist or not.
 function isExist(fileName) {
-    return filesys.existsSync(fileName);
+    return files.existsSync(fileName);
 }
 
 //function to read file
 function readFile(fileName) {
-    return filesys.readFileSync(fileName, "utf8");
+    return files.readFileSync(fileName, "utf8");
 }
 
 //function to split lines from file
@@ -46,12 +46,29 @@ function parseInputs() {
         }
     }
 }
+//initial function
+function init(fileName, lines, command) {
+    if (isExist(fileName)) {
+        if (!isNaN(lines)) {
+            const splitedData = splitData(readFile(fileName));
+            if (command == "head") {
+                printLines(splitedData.slice(0, lines));
+            }
+            if (command == "tail") {
+                printLines(splitedData.slice(-lines));
+            }
+        }
+        else {
+            console.log(`${command} : invalid number of lines: '${lines}'`);
+        }
 
+    }
+    else {
+        console.log(`${command} : cannot open '${fileName}' for reading: No such file or directory`);
+    }
+}
 //exporting functions 
 module.exports = {
-    isExist,
-    readFile,
-    splitData,
-    printLines,
+    init,
     parseInputs
 }
--- src/head.js
+++ src/head.js
@@ -1,20 +1,7 @@
-let files = require("./methods");
+let fileFunc = require("./fileFunctions");
 
-let userInput = files.parseInputs();
+let userInput = fileFunc.parseInputs();
 const fileName = userInput.fileName;
 const lines = userInput.lines;
 
-if (files.isExist(fileName)) {
-    if (!isNaN(lines)) {
-        const fileData = files.readFile(fileName);
-        const splitedData = files.splitData(fileData);
-        files.printLines(splitedData.slice(0, lines));
-    }
-    else {
-        console.log(`head: invalid number of lines: '${lines}'`);
-    }
-
-}
-else {
-    console.log(`head: cannot open '${fileName}' for reading: No such file or directory`);
-}
+fileFunc.init(fileName, lines, "head");
